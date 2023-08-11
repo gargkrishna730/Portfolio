7cad271e93dd5fcf012f9e9ed8c25f2e023a7a5d
PRITHVIBADIPURAM <202003039.prithvibhs@student.xavier.ac.in>
2023-08-11 12:29:01 +0530

    Linked the projects

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,19 +1,21 @@
 import React, { useContext } from "react";
 import "./Portfolio.css";
-import { Swiper, SwiperSlide } from "swiper/react"
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import Sidebar from "../../img/Unisex.png";
 import HOC from "../../img/news.png";
 import Ecommerce from "../../img/phone360.png";
 import MusicApp from "../../img/Music.png";
 import { themeContext } from "../../Context";
+
 const Portfolio = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
+
   return (
     <div className="portfolio" id="portfolio">
       {/* heading */}
-      <span style={{color: darkMode?'white': ''}}>Recent Projects</span>
+      <span style={{ color: darkMode ? "white" : "" }}>Recent Projects</span>
       <span>Portfolio</span>
 
       {/* slider */}
@@ -24,16 +26,24 @@ const Portfolio = () => {
         className="portfolio-slider"
       >
         <SwiperSlide>
-          <img src={Sidebar} alt="" />
+          <a href="https://unisexsalon.netlify.app/">
+            <img src={Sidebar} alt="" />
+          </a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Ecommerce} alt="" />
+          <a href="https://world-0f-crypto.netlify.app">
+            <img src={Ecommerce} alt="" />
+          </a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={MusicApp} alt="" />
+          <a href="https://sopity-clone.netlify.app/">
+            <img src={MusicApp} alt="" />
+          </a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={HOC} alt="" />
+          <a href="https://world-0f-crypto.netlify.app">
+            <img src={HOC} alt="" />
+          </a>
         </SwiperSlide>
       </Swiper>
     </div>
